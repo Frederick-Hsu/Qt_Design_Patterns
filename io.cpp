bef3b6f65de602ae03421ac1bbb0509b1907fdb8
Frederick Hsu <frederique.hsu@outlook.com>
2019-01-05 21:40:45 +0800

    Study the iostream input and output programming

---
+++ io.cpp
@@ -0,0 +1,33 @@
+/**************************************************************************************************
+ * File name    : io.cpp
+ * Description  : Study the iostream input and output.
+ * Creator      : Frederick Hsu
+ * Creation date: Sat.  05 Jan. 2019
+ * Copyright(C) 2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#include <string>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+    const int THISYEAR = 2019;
+    string yourName;
+    int birthYear;
+    
+    cout << "What is your name? " << flush;
+    cin >> yourName;
+    
+    cout << "What year were you born? ";
+    cin >> birthYear;
+    
+    cout << "Your name is " << yourName
+         << " and you are approximately "
+         << (THISYEAR - birthYear)
+         << " years old. " 
+         << endl;
+    
+    return 0;
+}

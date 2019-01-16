3b0c7a4b6603a85376fca504a5fc380ae0eca423
Frederique Hsu <frederique.hsu@outlook.com>
2019-01-17 01:58:11 +0800

    Define the functions prototype for std_stream and qt_stream.

---
+++ stream/stream.h
@@ -0,0 +1,16 @@
+/**************************************************************************************************
+ * File name    : stream.h
+ * Description  : Define the functions prototype about stream manipulation.
+ * Creator      : Frederique Hsu
+ * Creation date: Thu.  17 Jan. 2019
+ * Copyright(C) 2019    All rights reserved.
+ * 
+ **************************************************************************************************/
+
+#ifndef STREAM_H
+#define STREAM_H
+
+    void manipulate_std_stream(void);
+    void manipulate_qt_stream(void);
+
+#endif  /* STREAM_H */

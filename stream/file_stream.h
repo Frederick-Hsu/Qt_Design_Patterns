c1fb8addd998eb62d13afa466ff73ec1b1ef5976
Frederick Hsu <frederick.xu@amphenolmcp.com>
2019-01-17 11:24:11 +0800

    Define the function prototypes about manipulating the file stream.

---
+++ stream/file_stream.h
@@ -0,0 +1,15 @@
+/**************************************************************************************************
+ * File name    : file_stream.h
+ * Description  : Define the function prototypes about manipulating the file stream.
+ * Creator      : Frederick Hsu
+ * Creation date: Thu.  17 Jan. 2019
+ * Copyright(C) 2019    All rights reserved.
+ * 
+ **************************************************************************************************/
+
+#ifndef FILE_STREAM_H
+#define FILE_STREAM_H
+
+    void manipulate_std_file_stream(void);
+
+#endif  /* FILE_STREAM_H */

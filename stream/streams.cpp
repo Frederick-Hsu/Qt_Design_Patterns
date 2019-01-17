cf34905eb1a7e2294b3bc79edaad1a5753b6b3ce
Frederique Hsu <frederique.hsu@outlook.com>
2019-01-17 17:26:21 +0800

    Implement the function "void manipulate_std_file_stream(void)", to study how to read/write the file stream via ifstream and ofstream classes.

--- stream/streams.cpp
+++ stream/streams.cpp
@@ -33,4 +33,53 @@ void manipulate_std_file_stream(void)
     outf.open("mydata");
     outf << strval;
     outf.close();
+
+    cout << "Read data from the file - watch for errors." << endl;
+    string newstr;
+    ifstream inf;
+    inf.open("mydata");
+    if (inf)    /* Make sure the file exists before attempting to read. */
+    {
+        int lucky2;
+        inf >> newstr >> lucky2;
+        if (lucky != lucky2)
+        {
+            cerr << "ERROR! wrong " << newstr << lucky2 << endl;
+        }
+        else
+        {
+            cout << newstr << " OK" << endl;
+        }
+
+        float pi2;
+        inf >> newstr >> pi2;
+        if (pi2 != pi)
+            cerr << "ERROR! wrong " << newstr << pi2 << endl;
+        else
+            cout << newstr << " OK" << endl;
+        
+        double e2 ;
+        inf >> newstr >> e2;
+        if (e2 != e)
+        {
+            cerr << "ERROR : wrong " << newstr << e2 << endl;
+        }
+        else
+        {
+            cout << newstr << " OK" << endl;
+        }
+        inf.close();
+    }
+
+    cout << "Read from file line-by-line" << endl;
+    inf.open("mydata");
+    if (inf)
+    {
+        while (not inf.eof())
+        {
+            getline(inf, newstr);
+            cout << newstr << endl;
+        }
+        inf.close();
+    }
 }

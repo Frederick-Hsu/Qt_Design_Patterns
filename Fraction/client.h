21e826f9cc00eb4fa96bb5c92cd817816b6a1d0b
Frederique Hsu <frederique.hsu@outlook.com>
2019-01-24 01:05:44 +0800

    Design some test cases to verify user-defined structure and class.
    Invoke those functions of test cases in the main() function.

---
+++ Fraction/client.h
@@ -0,0 +1,16 @@
+/**************************************************************************************************
+ * File name    : client.h
+ * Description  : Define some test cases here.
+ * Creator      : Frederick Hsu
+ * Creation date: Thu.  24 Jan. 2019
+ * Copyright(C) 2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+    void accessStruture(void);
+    void accessClassFraction(void);
+
+#endif  /* CLIENT_H */

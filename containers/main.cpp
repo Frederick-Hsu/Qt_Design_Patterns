a7036b13c661e93ce67be720e0cca6d36ead7a08
Frederique Hsu (徐赞) <frederique.hsu@outlook.com>
2019-02-11 11:31:33 +0800

    Create a new project "containers.pro" to study the container topic in Qt C++ programming.

---
+++ containers/main.cpp
@@ -0,0 +1,20 @@
+/**************************************************************************************************
+ * Project      : containers
+ * Description  : Delve the topic of container in Qt C++.
+ * ================================================================================================
+ * File name    : main.cpp
+ * Description  : This file implements the main() entry function for current project.
+ * Creator      : Frederick Hsu
+ * Creation date: Mon.  11 Feb. 2019
+ * Copyright(C) 2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}

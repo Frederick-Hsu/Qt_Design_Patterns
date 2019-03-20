25e11f9b22a6c68a2ca4bf222ebea8789c5207c8
Frederique Hsu <frederique.hsu@outlook.com>
2019-03-20 17:32:57 +0800

    Create a new Qt console application project : Derivation.pro
    to study the inheritance topic of OOP.

---
+++ Inheritance_and_Polymorphism/Derivation/main.cpp
@@ -0,0 +1,21 @@
+/**************************************************************************************************
+ * Project      : Derivation
+ * Description  : Research the inheritance topic of OOP in the Qt context.
+ * ================================================================================================
+ * File name    : main.cpp
+ * Description  : This file implements the main() entry function for current project.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  20 March 2019
+ * Copyright(C) 2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}

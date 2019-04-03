069c742ca15e40a5c0cb1abcca48427a23e7b65a
Frederique Hsu <frederique.hsu@outlook.com>
2019-04-03 15:43:28 +0800

    Create a Qt console application project "Shapes.pro", to delve the abstract class design and concrete class inheritance.

---
+++ Inheritance_and_Polymorphism/Shapes/main.cpp
@@ -0,0 +1,21 @@
+/**************************************************************************************************
+ * Project      : Shapes
+ * Description  : Delve the abstract class design and concrete class inheritance.
+ * ================================================================================================
+ * File name    : main.cpp
+ * Description  : This file implements the main() entry function for current project.
+ * Creator      : Frederick Hsu
+ * Creation date: Wed.  03 April 2019
+ * Copyright(C) 2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}

ea97bac86b87e0d2808049093526a44dd10e32b5
Frederique Hsu <frederique.hsu@outlook.com>
2019-06-17 10:48:32 +0800

    Delve a comprehensive case on how to design the inheritance chain and the container.

---
+++ Inheritance_and_Polymorphism/LibraryApp/main.cpp
@@ -0,0 +1,21 @@
+/**************************************************************************************************
+ * Project      : LibraryApp
+ * Description  : Construct a comprehensive application to summary up the inheritance and polymorphism.
+ * ================================================================================================
+ * File name    : main.cpp
+ * Description  : This file implements the entry function main() for current project.
+ * Creator      : Frederique Hsu
+ * Creation date: Fri.  24 May 2019
+ * Copyright(C) 2019    All rights reserved.
+ *
+ **************************************************************************************************/
+
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}

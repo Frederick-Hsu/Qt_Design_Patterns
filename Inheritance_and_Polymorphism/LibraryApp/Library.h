bb6caae5acd90c1c66bb81c11c2ac1a155445d56
Frederique Hsu <frederique.hsu@outlook.com>
2019-07-25 13:55:38 +0800

    Define 2 inheritance paradigm for the class Library, and allow programmer to select one from them.

--- Inheritance_and_Polymorphism/LibraryApp/Library.h
+++ Inheritance_and_Polymorphism/LibraryApp/Library.h
@@ -2,8 +2,15 @@
 #define LIBRARY_H
 
     #include <QList>
+    #include <QMap>
+
+    #define QLIST_INHERITANCE   1
+    #define QMAP_INHERITANCE    2
+    #define INHERITANCE_PARADIGM    QLIST_INHERITANCE
+
     class RefItem;
 
+#if (INHERITANCE_PARADIGM == QLIST_INHERITANCE)
     class Library : public QList<RefItem*>
     {
     public:
@@ -20,5 +27,13 @@
         Library& operator=(const Library&);     /* copy assignment constructor */
         RefItem* findRefItem(QString isbn);
     };
+#elif (INHERITANCE_PARADIGM == QMAP_INHERITANCE)
+    class Library : public QMap<QString, RefItem*>
+    {
+    public:
+        Library();
+        ~Library();
+    };
+#endif
 
 #endif  /* LIBRARY_H */

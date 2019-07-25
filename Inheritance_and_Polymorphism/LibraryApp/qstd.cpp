c58ce06cc4a4ea3249903b8753f9eec3bc9f9aca
Frederique Hsu <frederique.hsu@outlook.com>
2019-07-25 11:44:17 +0800

    add the utility standalone functions : qstd::cout, qstd::cin and qstd::cerr

---
+++ Inheritance_and_Polymorphism/LibraryApp/qstd.cpp
@@ -0,0 +1,5 @@
+#include "qstd.h"
+
+QTextStream qstd::cout(stdout, QIODevice::WriteOnly);
+QTextStream qstd::cin(stdin, QIODevice::ReadOnly);
+QTextStream qstd::cerr(stderr, QIODevice::WriteOnly);
---
+++ Inheritance_and_Polymorphism/LibraryApp/qstd.h
@@ -0,0 +1,14 @@
+#ifndef QSTD_H
+#define QSTD_H
+
+    #include <QTextStream>
+
+    namespace qstd
+    {
+        /* declared but not implemented */
+        extern QTextStream cout;
+        extern QTextStream cin;
+        extern QTextStream cerr;
+    };
+
+#endif  /* QSTD_H */

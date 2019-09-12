c9af9aaf3aee67c7d1ebd398f4a86504b0548eff
Frederique Hsu <frederique.hsu@outlook.com>
2019-09-13 02:41:25 +0800

    Design the UI by using space and stretch, to demonstrate how they control the layout.

--- Poker_Cards_Game/cards2d_static/CardTable.cpp
+++ Poker_Cards_Game/cards2d_static/CardTable.cpp
@@ -5,8 +5,13 @@
 #include <QVBoxLayout>
 #include <QPushButton>
 
+#define WINDOW_LAYOUT_WITHOUT_STRETCH_SPACING   1
+#define WINDOW_LAYOUT_WITH_STRETCH_SPACING      2
+#define WINDLOW_LAYOUT                          WINDOW_LAYOUT_WITH_STRETCH_SPACING
+
 CardTable::CardTable(QWidget* parent) : QWidget(parent)
 {
+#if (WINDLOW_LAYOUT == WINDOW_LAYOUT_WITHOUT_STRETCH_SPACING)
     QHBoxLayout* first_row = new QHBoxLayout();
     first_row->addWidget(new Card("ah"));
     first_row->addWidget(new Card("qd"));
@@ -30,4 +35,34 @@ CardTable::CardTable(QWidget* parent) : QWidget(parent)
     setLayout(cols);
     cols->addLayout(rows);
     cols->addLayout(buttons);
+#elif (WINDLOW_LAYOUT == WINDOW_LAYOUT_WITH_STRETCH_SPACING)
+    QHBoxLayout* first_low = new QHBoxLayout();
+    first_low->addWidget(new Card("as"));
+    first_low->addWidget(new Card("ah"));
+    first_low->addWidget(new Card("ad"));
+    first_low->addWidget(new Card("ks"));
+
+    QHBoxLayout* second_row = new QHBoxLayout();
+    second_row->addWidget(new Card("ks"));
+    second_row->addWidget(new Card("kh"));
+    second_row->addWidget(new Card("kd"));
+
+    QVBoxLayout* rows = new QVBoxLayout();
+    rows->addLayout(first_low);
+    rows->addLayout(second_row);
+    rows->addStretch(1);
+
+    QVBoxLayout* buttons = new QVBoxLayout();
+    buttons->addStretch(1);
+    buttons->addWidget(new QPushButton(tr("Deal")));
+    buttons->addWidget(new QPushButton(tr("Shuffle")));
+    buttons->addSpacing(20);
+
+    QHBoxLayout* cols = new QHBoxLayout();
+    setLayout(cols);
+
+    cols->addLayout(rows);
+    cols->addLayout(buttons);
+    cols->addStretch(0);
+#endif
 }

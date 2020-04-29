58ccd99e265a88a5888d968694aabf68a65c7ef0
Frederique Hsu <frederique.hsu@outlook.com>
2020-04-29 16:59:56 +0800

    Create a sub-project: Factory_Pattern, to study how to write it, and try to figure out how this Factory Pattern works.

---
+++ Design_Patterns/Factory_Pattern/Factory_Pattern.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        Simple_Factory/CarBase.cpp \
+        main.cpp
+
+HEADERS += \
+    Simple_Factory/CarBase.h
---
+++ Design_Patterns/Factory_Pattern/main.cpp
@@ -0,0 +1,20 @@
+/* Project name : Factory_Pattern
+ * Description  : Make an in-depth research on the factory pattern, learn how it works.
+ * =================================================================================================
+ * File name    : main.cpp
+ * Description  : This file implements the main() entry function for current project.
+ * Creator      : Frederique Hsu
+ * Creation date: Wed.  29 April 2020
+ * Copyright(C) 2020    All rights reserved.
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "C++ standard no.: " << __cplusplus << endl;
+    return 0;
+}
